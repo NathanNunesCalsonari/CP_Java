f8bd365c43cd2b8bc907e3ffca1748b8546b0159
NathanNunesCalsonari <130010914+NathanNunesCalsonari@users.noreply.github.com>
2024-09-01 20:03:53 -0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# Estágio de Build
+# Use uma imagem base do Ubuntu para construção
 FROM ubuntu:latest AS build
 
 # Atualize o repositório e instale o JDK e Maven
@@ -14,7 +14,7 @@ COPY src ./src
 # Execute a construção do Maven ignorando os testes
 RUN mvn clean install -DskipTests
 
-# Estágio Final
+# Use uma imagem base mais leve para executar o aplicativo
 FROM openjdk:17-jdk-slim
 
 # Expor a porta 8080 (ajuste se sua aplicação usa uma porta diferente)
@@ -24,7 +24,7 @@ EXPOSE 8080
 WORKDIR /app
 
 # Copie o arquivo JAR gerado para a imagem final
-COPY --from=build /app/target/lojadebrinquedo-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=build /app/target/lojadebrinquedo.jar app.jar
 
 # Comando para executar o aplicativo
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/br/com/lojadebrinquedo/controller/BrinquedoController.java
+++ src/main/java/br/com/lojadebrinquedo/controller/BrinquedoController.java
@@ -2,17 +2,17 @@ package br.com.lojadebrinquedo.controller;
 
 import br.com.lojadebrinquedo.dto.BrinquedoDTO;
 import br.com.lojadebrinquedo.service.BrinquedoService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/brinquedos")
 @RequiredArgsConstructor
 public class BrinquedoController {
@@ -21,38 +21,51 @@ public class BrinquedoController {
     private BrinquedoService brinquedoService;
 
     @GetMapping("/listar")
-    public List<BrinquedoDTO> listarBrinquedos() {
-        return brinquedoService.listarTodosBrinquedos();
+    public String listarBrinquedos(Model model) {
+        List<BrinquedoDTO> brinquedos = brinquedoService.listarTodosBrinquedos();
+        model.addAttribute("brinquedos", brinquedos);
+        return "listar";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BrinquedoDTO> obterBrinquedoPorId(@PathVariable Long id) {
-        Optional<BrinquedoDTO> brinquedo = brinquedoService.encontrarBrinquedoPorId(id);
-        return brinquedo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/novo")
+    public String exibirFormularioCadastro(Model model) {
+        model.addAttribute("brinquedo", new BrinquedoDTO());
+        model.addAttribute("titulo", "Cadastrar Novo Brinquedo");
+        return "form";
     }
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<BrinquedoDTO> cadastrarBrinquedo(@Valid @RequestBody BrinquedoDTO brinquedoDTO) {
-        BrinquedoDTO novoBrinquedo = brinquedoService.salvarBrinquedo(brinquedoDTO);
-        return ResponseEntity.ok(novoBrinquedo);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<BrinquedoDTO> atualizarBrinquedo(@PathVariable Long id,
-                                                           @Valid @RequestBody BrinquedoDTO brinquedoDTO) {
+    @GetMapping("/editar/{id}")
+    public String exibirFormularioEdicao(@PathVariable Long id, Model model) {
         Optional<BrinquedoDTO> brinquedo = brinquedoService.encontrarBrinquedoPorId(id);
         if (brinquedo.isPresent()) {
-            brinquedoDTO.setId(id);
-            BrinquedoDTO brinquedoAtualizado = brinquedoService.salvarBrinquedo(brinquedoDTO);
-            return ResponseEntity.ok(brinquedoAtualizado);
+            model.addAttribute("brinquedo", brinquedo.get());
+            model.addAttribute("titulo", "Editar Brinquedo");
+            return "form";
         } else {
-            return ResponseEntity.notFound().build();
+            return "redirect:/brinquedos/listar";
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarBrinquedo(@PathVariable Long id) {
+    @PostMapping("/salvar")
+    public String salvarBrinquedo(@Valid @ModelAttribute BrinquedoDTO brinquedoDTO) {
+        brinquedoService.salvarBrinquedo(brinquedoDTO);
+        return "redirect:/brinquedos/listar";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String exibirConfirmacaoDelecao(@PathVariable Long id, Model model) {
+        Optional<BrinquedoDTO> brinquedo = brinquedoService.encontrarBrinquedoPorId(id);
+        if (brinquedo.isPresent()) {
+            model.addAttribute("brinquedo", brinquedo.get());
+            return "confirmarDelecao";
+        } else {
+            return "redirect:/brinquedos/listar";
+        }
+    }
+
+    @PostMapping("/deletar/{id}")
+    public String deletarBrinquedo(@PathVariable Long id) {
         brinquedoService.deletarBrinquedo(id);
-        return ResponseEntity.noContent().build();
+        return "redirect:/brinquedos/listar";
     }
 }
